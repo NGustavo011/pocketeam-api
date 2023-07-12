e985481d271755743149c905f5c4db610449995b
NGustavo011 <gsn181200@gmail.com>
2023-07-12 10:50:47 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+export default {
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  preset: '@shelf/jest-mongodb',
+  roots: [
+    '<rootDir>/src'
+  ],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
