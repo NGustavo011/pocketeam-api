ef28f5d6526437e54b0892f42382271d09cde7d8
NGustavo011 <gsn181200@gmail.com>
2023-07-12 16:11:35 -0300

    feat: add authentication usecase contract

---
+++ src/domain/usecases-contracts/account/authentication.ts
@@ -0,0 +1,10 @@
+import { type AuthenticationModel } from '../../models/authentication'
+
+export interface AuthenticationParams {
+  email: string
+  password: string
+}
+
+export interface Authentication {
+  auth: (authenticationParams: AuthenticationParams) => Promise<AuthenticationModel | null>
+}
