32069f5fc4a8a751f7d286dcdb9aa512535f125e
NGustavo011 <gsn181200@gmail.com>
2023-07-12 11:18:04 -0300

    chore: init swagger

---
+++ src/main/docs/swagger/swagger-config.ts
@@ -0,0 +1,11 @@
+export const swaggerConfig = {
+  openapi: '3.0.0',
+  info: {
+    title: 'PockeTeam API',
+    description: 'API desenvolvida para fãs de pokémon criarem os seus times ideais, seja times competitivos, temáticos ou apenas casuais.',
+    version: '1.0.0'
+  },
+  servers: [{
+    url: '/api'
+  }]
+}
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,9 +1,12 @@
 import express from 'express'
 import cors from 'cors'
 import { loadEnvConfig } from './config/env'
+import { serve, setup } from 'swagger-ui-express'
+import { swaggerConfig } from './docs/swagger/swagger-config'
 
 loadEnvConfig()
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use('/api-docs', serve, setup(swaggerConfig))
 app.listen(process.env.PORT ?? 3333, () => { console.log('Server is running!') })
