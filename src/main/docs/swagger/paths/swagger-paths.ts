09b7cf0634c819f96a45234918af780d3874d352
NGustavo011 <gsn181200@gmail.com>
2023-07-12 11:38:02 -0300

    docs: add login route to swagger

--- src/main/docs/swagger/paths/swagger-paths.ts
+++ src/main/docs/swagger/paths/swagger-paths.ts
@@ -1,5 +1,7 @@
+import { loginPath } from './login-path'
 import { signUpPath } from './signup-path'
 
 export const swaggerPaths = {
+  '/login': loginPath,
   '/signup': signUpPath
 }
--- src/main/docs/swagger/schemas/swagger-schemas.ts
+++ src/main/docs/swagger/schemas/swagger-schemas.ts
@@ -1,11 +1,13 @@
 import { accountSchema } from './account-schema'
 import { apiKeyAuthSchema } from './api-key-auth-schema'
 import { errorSchema } from './error-schema'
+import { loginParamsSchema } from './login-params-schema'
 import { signUpParamsSchema } from './signup-params-schema'
 
 export const swaggerSchemas = {
   account: accountSchema,
   signUpParams: signUpParamsSchema,
+  loginParams: loginParamsSchema,
   error: errorSchema,
   apiKeyAuth: apiKeyAuthSchema
 }
