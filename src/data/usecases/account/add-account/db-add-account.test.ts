24327a1b66ffdcbf8891f3e6bbad9b2e57109f83
NGustavo011 <gsn181200@gmail.com>
2023-07-12 15:32:06 -0300

    feat: ensure DbAddAccount throws if Hasher throws

--- src/data/usecases/account/add-account/db-add-account.test.ts
+++ src/data/usecases/account/add-account/db-add-account.test.ts
@@ -1,3 +1,4 @@
+import { throwError } from '../../../../domain/test/test-helpers'
 import { type Hasher } from '../../../repositories-contracts/cryptography/hasher'
 import { mockHasher } from '../../../test/mock-cryptography'
 import { mockAddAccountParams } from '../../../test/mock-db-account'
@@ -23,5 +24,11 @@ describe('DbAddAccount usecase', () => {
     const hashSpy = jest.spyOn(hasherStub, 'hash')
     await sut.add(mockAddAccountParams())
     expect(hashSpy).toHaveBeenCalledWith(mockAddAccountParams().password)
+  })
+  test('Deve propagar o erro caso o Hasher lance um erro', async () => {
+    const { sut, hasherStub } = makeSut()
+    jest.spyOn(hasherStub, 'hash').mockImplementationOnce(throwError)
+    const promise = sut.add(mockAddAccountParams())
+    await expect(promise).rejects.toThrow()
   })
 })
