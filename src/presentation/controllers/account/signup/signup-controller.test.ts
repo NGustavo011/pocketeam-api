fc62b2de3c9e6ee91ca754a5d51152c19287373e
NGustavo011 <gsn181200@gmail.com>
2023-07-14 17:40:15 -0300

    feat: ensure SignUpController calls Validation with correct params

--- src/presentation/controllers/account/signup/signup-controller.test.ts
+++ src/presentation/controllers/account/signup/signup-controller.test.ts
@@ -1,6 +1,8 @@
 import { throwError } from '../../../../domain/test/test-helpers'
 import { type AddAccountContract } from '../../../../domain/usecases-contracts/account/add-account'
+import { mockValidation } from '../../../../validation/test/mock-validation'
 import { type HttpRequest } from '../../../contracts/http'
+import { type Validation } from '../../../contracts/validation'
 import { EmailInUseError, ServerError } from '../../../errors'
 import { forbidden, serverError } from '../../../helpers/http/http-helper'
 import { mockAddAccount } from '../../../test/mock-account'
@@ -20,37 +22,50 @@ const mockRequest = (): HttpRequest => {
 interface SutTypes {
   sut: SignUpController
   addAccountStub: AddAccountContract
+  validationStub: Validation
 }
 
 const makeSut = (): SutTypes => {
   const addAccountStub = mockAddAccount()
-  const sut = new SignUpController(addAccountStub)
+  const validationStub = mockValidation()
+  const sut = new SignUpController(addAccountStub, validationStub)
   return {
-    sut, addAccountStub
+    sut, addAccountStub, validationStub
   }
 }
 
 describe('SignUp Controller', () => {
-  test('Deve chamar AddAccount utilizando os valores corretos', async () => {
-    const { sut, addAccountStub } = makeSut()
-    const addSpy = jest.spyOn(addAccountStub, 'add')
-    await sut.execute(mockRequest())
-    expect(addSpy).toHaveBeenCalledWith({
-      name: 'any_name',
-      email: 'any_mail@mail.com',
-      password: 'any_password'
+  describe('AddAccount dependency', () => {
+    test('Deve chamar AddAccount utilizando os valores corretos', async () => {
+      const { sut, addAccountStub } = makeSut()
+      const addSpy = jest.spyOn(addAccountStub, 'add')
+      await sut.execute(mockRequest())
+      expect(addSpy).toHaveBeenCalledWith({
+        name: 'any_name',
+        email: 'any_mail@mail.com',
+        password: 'any_password'
+      })
+    })
+    test('Retorne status de erro 500 se o handle lançar um erro', async () => {
+      const { sut, addAccountStub } = makeSut()
+      jest.spyOn(addAccountStub, 'add').mockImplementationOnce(throwError)
+      const httpResponse = await sut.execute(mockRequest())
+      expect(httpResponse).toEqual(serverError(new ServerError()))
+    })
+    test('Retorne status 403 se o AddAccount retornar null', async () => {
+      const { sut, addAccountStub } = makeSut()
+      jest.spyOn(addAccountStub, 'add').mockReturnValueOnce(new Promise(resolve => { resolve(null) }))
+      const httpResponse = await sut.handle(mockRequest())
+      expect(httpResponse).toEqual(forbidden(new EmailInUseError()))
     })
   })
-  test('Retorne status de erro 500 se o handle lançar um erro', async () => {
-    const { sut, addAccountStub } = makeSut()
-    jest.spyOn(addAccountStub, 'add').mockImplementationOnce(throwError)
-    const httpResponse = await sut.execute(mockRequest())
-    expect(httpResponse).toEqual(serverError(new ServerError()))
-  })
-  test('Retorne status 403 se o AddAccount retornar null', async () => {
-    const { sut, addAccountStub } = makeSut()
-    jest.spyOn(addAccountStub, 'add').mockReturnValueOnce(new Promise(resolve => { resolve(null) }))
-    const httpResponse = await sut.handle(mockRequest())
-    expect(httpResponse).toEqual(forbidden(new EmailInUseError()))
+  describe('Validation dependency', () => {
+    test('Deve chamar o Validation com valores corretos', async () => {
+      const { sut, validationStub } = makeSut()
+      const validateSpy = jest.spyOn(validationStub, 'validate')
+      const httpRequest = mockRequest()
+      await sut.handle(httpRequest)
+      expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+    })
   })
 })
--- src/presentation/controllers/account/signup/signup-controller.ts
+++ src/presentation/controllers/account/signup/signup-controller.ts
@@ -1,15 +1,17 @@
 import { type AddAccountContract } from '../../../../domain/usecases-contracts/account/add-account'
 import { Controller } from '../../../contracts/controller'
 import { type HttpRequest, type HttpResponse } from '../../../contracts/http'
+import { type Validation } from '../../../contracts/validation'
 import { EmailInUseError } from '../../../errors'
 import { forbidden } from '../../../helpers/http/http-helper'
 
 export class SignUpController extends Controller {
-  constructor (private readonly addAccount: AddAccountContract) {
+  constructor (private readonly addAccount: AddAccountContract, private readonly validation: Validation) {
     super()
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    this.validation.validate(httpRequest.body)
     const { name, email, password } = httpRequest.body
     const account = await this.addAccount.add({
       name,
