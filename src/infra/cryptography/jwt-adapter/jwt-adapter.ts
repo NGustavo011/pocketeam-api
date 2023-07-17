b5a13c7bcc8943061de2ed9eb60cad1329dd2755
NGustavo011 <gsn181200@gmail.com>
2023-07-17 13:55:18 -0300

    feat: implement DecrypterRepository in JwtAdapter

--- src/infra/cryptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.ts
@@ -1,10 +1,20 @@
 import jwt from 'jsonwebtoken'
 import { type EncrypterRepository } from '../../../data/repositories-contracts/cryptography/encrypter-repository'
+import { type DecrypterRepository } from '../../../data/repositories-contracts/cryptography/decrypter-repository'
 
-export class JwtAdapter implements EncrypterRepository {
+export class JwtAdapter implements EncrypterRepository, DecrypterRepository {
   constructor (private readonly secret: string) {}
   async encrypt (id: string): Promise<string> {
     const accessToken = jwt.sign({ id }, this.secret)
     return await new Promise(resolve => { resolve(accessToken) })
+  }
+
+  async validateToken (token: string): Promise<boolean> {
+    try {
+      jwt.verify(token, this.secret)
+      return true
+    } catch (error) {
+      return false
+    }
   }
 }
